dd8c75b3c8c7637e8f01420e87fdf4ec6e618645
kcmurray1 <kristiancmurray@gmail.com>
2024-08-02 08:42:31 -0700

    setup build and run to android device

---
+++ Unity Rhythm Game 2024/Assets/Scripts/touch_controls/TouchManager.cs
@@ -0,0 +1,80 @@
+using System;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _JudgementLine;
+    //Camera to base touches on
+    [SerializeField] private Camera CameraMain; 
+    //private GameObject player;
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private float YoffSet;
+    [SerializeField] private float XoffSet;
+
+    [SerializeField] private GameObject TouchObject;
+
+    //USED FOR DEBUGGING
+    [SerializeField] private GameObject TouchObjectTwo;
+    private InputAction touchPositionAction2;
+    //END OF DEBUGGING
+    //Action that receives the position of a touch
+    private InputAction touchPositionAction;
+    //Action that waits to receive touch
+    private InputAction touchPressAction;
+
+    private InputAction touchPressAction2;
+    private Vector3 position;
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        touchPressAction = playerInput.actions["TouchPress"];
+        touchPositionAction = playerInput.actions["TouchPosition"];
+        touchPositionAction2 = playerInput.actions["TouchPosition2"];
+        touchPressAction2 = playerInput.actions["TouchPress2"];
+    }
+    private void OnEnable()
+    {
+        touchPressAction.started += TouchPressed;
+        touchPressAction.canceled += ResetPosition;
+        touchPressAction2.started += TouchPressedTwo;
+        touchPressAction2.canceled += ResetPosition2;
+    }
+
+    private void OnDisable()
+    {
+        touchPressAction.started -= TouchPressed;
+        touchPressAction.canceled -= ResetPosition;
+        touchPressAction2.started -= TouchPressedTwo;
+        touchPressAction2.canceled -= ResetPosition2;
+    }
+    private void ResetPosition(InputAction.CallbackContext context)
+    {
+        TouchObject.transform.position = new Vector3(0f, 0f, 0f);
+    }
+    private void ResetPosition2(InputAction.CallbackContext context)
+    {
+        TouchObjectTwo.transform.position = new Vector3(0f, 0f, 0f);
+    }
+
+    private Vector3 ScreenToWorldPosition(Vector3 touchPosition)
+    {
+        //Convert touch position to position on the Camera screen.
+        position = CameraMain.ScreenToWorldPoint(new Vector3(touchPosition.x, touchPosition.y, 0f));
+        return new Vector3(position.x, position.y, 0f);
+    }
+
+    private void TouchPressedTwo(InputAction.CallbackContext context)
+    {
+        TouchObjectTwo.transform.position = ScreenToWorldPosition(touchPositionAction.ReadValue<Vector2>());
+    }
+    //Destroy object when pressed
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+        TouchObject.transform.position = ScreenToWorldPosition(touchPositionAction.ReadValue<Vector2>());
+        Debug.Log("Touch position" + TouchObject.transform.position);
+        
+    }
+
+}
